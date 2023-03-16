13846d487eccfd5505be10a2b9b7f6bb1d8b2fc1
Wedlec129 <boryasokolov@gmail.com>
2023-03-16 13:14:02 +0300

    add

--- dicee/ViewController.swift
+++ dicee/ViewController.swift
@@ -9,11 +9,33 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    //первый кубик
+    @IBOutlet weak var cubeOne: UIImageView!
+   
+    //второй кубик
+    @IBOutlet weak var cubeTwo: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+ //если нажали на кнопку
+    @IBAction func button(_ sender: Any) {
+        print("button pressed")
+        var image=[UIImage(imageLiteralResourceName:"DiceOne"),
+                     UIImage(imageLiteralResourceName:"DiceTwo"),
+                     UIImage(imageLiteralResourceName:"DiceThree"),
+                     UIImage(imageLiteralResourceName:"DiceFour"),
+                     UIImage(imageLiteralResourceName:"DiceFive"),
+                     UIImage(imageLiteralResourceName:"DiceSix"),
+                       
+                     ]
+        
+        cubeOne.image=image.randomElement()
+        cubeTwo.image=image[Int.random(in: 0...image.count-1)]
+       
+        
+    }
 }
 
